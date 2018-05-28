f9c6bf3f9bdc902ecc7686e7c6fc51236b2c17f0
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-28 16:00:19 +0200

    Update Contacts.js file's content

--- components/Contacts.js
+++ components/Contacts.js
@@ -1,26 +1,44 @@
-/*var Contacts = React.createClass({
-  propTypes: {
-    items: React.PropTypes.array.isRequired,
-  },
-
-  render: function() {
-    var contacts = this.props.items.map(function(contact) {
-      return React.createElement(Contact, {item: contact, key: contact.id});
-    });
-    return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
-    );
-  }
-});*/
 
 class Contacts extends React.Component {
-  constructor(items) {
-    super(items);
-  }
-  render() {
-      var contacts = this.props.items.map(function(contact){
-        return (<Contact item = {contact} key = {contact.id}></Contact>);
-      });
-      return (<ul className={'contactsList'}>{contacts}</ul>);
+
+      getContacts() {
+        return this.props.contacts.map(({id = 0, firstName = '', lastName= '', secondLastName ='', email =''}) => {
+         <li key={id}>
+         <div className={'contactItem'}>
+               <img className={'contactImage'} src={'./components/contact.png'}/>
+               <p className={'contactLabel'}>
+               Nombre:
+                 <span className={'contactLabel_span'}>
+                   {firstName}
+                 </span>
+               </p>
+               <p className={'contactLabel'}>
+               Primer apellido:
+                 <span className={'contactLabel_span'}>
+                   {lastName}
+                 </span>
+               </p>
+               <p className={'contactLabel'}>
+               Segundo apellido:
+                 <span className={'contactLabel_span'}>
+                   {secondLastName}
+                 </span>
+               </p>
+               <p className={'contactEmail'}>
+               Correo:
+                 <a className={'contactEmail_link'} href={'mailto:' + this.props.item.email}>
+                 {email}
+                 </a>
+               </p>
+           </div>
+         </li>
+       });
+   }
+    render() {
+      return (
+        <ul>
+          {this.getContacts()}
+        </ul>
+      )
     }
   }
