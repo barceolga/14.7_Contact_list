a305bf53f2d3d7559454b9805cbc6aa3bec81ca8
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-31 20:49:47 +0200

    Update content for Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -35,26 +35,26 @@ class Contact extends React.Component {
       <img className={'contactImage'} src={'./components/contact.png'}/>
       <p className={'contactLabel'}>
       Nombre:
-        <span className={'contactLabel_span'}>
-          {this.props.item.firstName}
+      <span className={'contactLabel_span'}>
+           {this.props.item.firstName}
         </span>
       </p>
       <p className={'contactLabel'}>
       Primer apellido:
-        <span className={'contactLabel_span'}>
-          {this.props.item.lastName}
+      <span className={'contactLabel_span'}>
+           {this.props.item.lastName}
         </span>
       </p>
       <p className={'contactLabel'}>
       Segundo apellido:
-        <span className={'contactLabel_span'}>
-          {this.props.item.secondLastName}
+      <span className={'contactLabel_span'}>
+           {this.props.item.secondLastName}
         </span>
       </p>
       <p className={'contactEmail'}>
       Correo:
-        <a className={'contactEmail_link'} href={'mailto:' + this.props.item.email}>
-        {this.props.item.email}
+      <a className={'contactEmail_link'} href={'mailto:' + this.props.item.email}>
+         {this.props.item.email}
         </a>
       </p>
     </div>
