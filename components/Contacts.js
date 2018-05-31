2986ad6f9f6c46d90c5edb76d70096f5bfe78b8b
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-05-31 20:42:57 +0200

    Fix bugs in Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -2,41 +2,14 @@
 class Contacts extends React.Component {
 
       getContacts() {
-        return this.props.contacts.map(({id = 0, firstName = '', lastName= '', secondLastName ='', email =''}) => {
-         <li key={id}>
-         <div className={'contactItem'}>
-               <img className={'contactImage'} src={'./components/contact.png'}/>
-               <p className={'contactLabel'}>
-               Nombre:
-                 <span className={'contactLabel_span'}>
-                   {firstName}
-                 </span>
-               </p>
-               <p className={'contactLabel'}>
-               Primer apellido:
-                 <span className={'contactLabel_span'}>
-                   {lastName}
-                 </span>
-               </p>
-               <p className={'contactLabel'}>
-               Segundo apellido:
-                 <span className={'contactLabel_span'}>
-                   {secondLastName}
-                 </span>
-               </p>
-               <p className={'contactEmail'}>
-               Correo:
-                 <a className={'contactEmail_link'} href={'mailto:' + this.props.item.email}>
-                 {email}
-                 </a>
-               </p>
-           </div>
-         </li>
+        return this.props.contacts.map(item => {
+         return <Contact item={item} key={item.id} />
+
        });
    }
     render() {
       return (
-        <ul>
+        <ul className={'contactsList'}>
           {this.getContacts()}
         </ul>
       )
