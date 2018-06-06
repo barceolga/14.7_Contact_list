7742c1f4c22cee4cf262a78690ee9201f08852e5
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-06-06 12:39:54 +0200

    Update content for Contact.js file

--- components/Contact.js
+++ components/Contact.js
@@ -1,28 +1,4 @@
-/*var Contact = React.createClass({
-  propTypes: {
-    item: React.PropTypes.object.isRequired
-  },
 
-  render: function() {
-    return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: './components/contact.png'
-        }),
-          React.createElement('p', {className: 'contactLabel'}, 'Nombre ',
-            React.createElement('span', {className: 'contactLabel_span'}, this.props.item.firstName)),
-          React.createElement('p', {className: 'contactLabel'}, 'Primer apellido ',
-            React.createElement('span', {className: 'contactLabel_span'}, this.props.item.lastName)),
-          React.createElement('p', {className: 'contactLabel'}, 'Segundo apellido ',
-            React.createElement('span', {className: 'contactLabel_span'}, this.props.item.secondLastName)),
-          React.createElement('p', {className: 'contactEmail'}, 'Correo ',
-            React.createElement('a', {className: 'contactEmail_link', href: 'mailto:' + this.props.item.email}, this.props.item.email)
-        )
-      )
-    )
-  },
-});*/
 
 class Contact extends React.Component {
   constructor(props) {
@@ -56,6 +32,12 @@ class Contact extends React.Component {
       <a className={'contactEmail_link'} href={'mailto:' + this.props.item.email}>
          {this.props.item.email}
         </a>
+      </p>
+      <p className={'contactLabel'}>
+      Teléfono móvil:
+      <span className={'contactLabel_span'}>
+           {this.props.item.mobile}
+        </span>
       </p>
     </div>
     );
